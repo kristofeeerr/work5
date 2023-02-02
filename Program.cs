5d83f1df1d1116395920dae0fc0f055af6fa62b2
kristofeeerr <123837931+kristofeeerr@users.noreply.github.com>
2023-02-03 01:32:28 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Aleksey";
+            int age = 21;
+            var zodiacSign = "Aquarius";
+            bool worksFactory = true;
+            Console.WriteLine($"Вас зовут {name}. Вам {age} год. Ваш знак зодиака {zodiacSign}. " +
+            $"Вы работаете на заводе - {worksFactory}.");
+        }
+    }
+}
